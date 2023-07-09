8d7d04a126187a2aa6f026aa1de8b7c3c2e6b8a7
abuobeyada <abuobeyada574@gmail.com>
2023-07-09 23:02:41 +0600

    facebook timeline

---
+++ app/data.js
@@ -0,0 +1,31 @@
+const users = [
+  {
+    id: 1,
+    image:
+      "https://scontent.fdac14-1.fna.fbcdn.net/v/t39.30808-6/357728476_999872584540870_356447128904779318_n.jpg?stp=dst-jpg_p843x403&_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFAPY83KL44sJhUblIFPsDqZL7t1jiBOttkvu3WOIE626WiAF4WzKD4NR_AOq0ekdPP40rq-WYiKoGitO7Rsu4M&_nc_ohc=A2JFZFXUsd0AX8VRmkJ&_nc_ht=scontent.fdac14-1.fna&oh=00_AfCZnh8veCXv0fliSngYnHF8csUl5qALAHwhmwU3i0vusQ&oe=64A87042",
+    content:
+      "A friend is someone who knows all your flaws and still chooses to be by your side.",
+    like: 500,
+    comment: 20,
+    timeAgo: "20 min",
+    author: {
+      name: "MD Ruhul Amin Tarafdar",
+      image:
+        "https://scontent.fdac14-1.fna.fbcdn.net/v/t39.30808-6/339412524_181574138028723_7703484005793584859_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEeUAJqjhCQl-JeujrNNiv7vnp-aoF74Ua-en5qgXvhRur8QwSmQDFkvOi2SVFGXf5U2zyZ0PBR_KX341Yh053q&_nc_ohc=iiMybhringcAX_MWMxT&_nc_ht=scontent.fdac14-1.fna&oh=00_AfB7o0G98vYecZUtjkuTnN4aT_fPO0kBgIxk-ym5QzlgFQ&oe=64A9934B",
+    },
+  },
+  {
+    id: 2,
+    image:
+      "https://scontent.fdac14-1.fna.fbcdn.net/v/t39.30808-6/356705553_1392155421567251_5254785080282242934_n.jpg?stp=cp6_dst-jpg_p526x296&_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHGWVPwreu0EwBOuWeinvcssRkCBVx5GxuxGQIFXHkbG0siir-qv9GQK8tgD5x_EdnVyunF9ZygwKVA4RWPAKez&_nc_ohc=WXTDrcClaogAX9L4pzW&_nc_ht=scontent.fdac14-1.fna&oh=00_AfAMoBSrH6Y2Zfx5Si4AZUE6jzc9DR0hc_kiznQ2VYfSuQ&oe=64A99B32",
+    content: `মিষ্টি পান 🥘 খাইতে গেছিলাম ভাই সেই স্বাদ।`,
+    like: 900,
+    comment: 100,
+    timeAgo: "2 hours",
+    author: {
+      name: "Adnan Habib Nayeem",
+      image:
+        "https://www.facebook.com/photo/?fbid=1392095434906583&set=a.120584088724397&__tn__=%3C",
+    },
+  },
+];
